49cd410d94f685f0e4222c774942765d18e5e150
andre059 <andreyperm159@mail.ru>
2024-01-17 17:08:33 +0500

    поправки в модели default='null' в поле phone_number, для успешной миграции

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from users.models import CustomUser
+from users.models import User
 
 
-@admin.register(CustomUser)
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'phone_number', 'first_name', 'last_name', 'referred_by')
     list_filter = ('first_name', 'last_name', 'phone_number')
--- users/models.py
+++ users/models.py
@@ -6,11 +6,11 @@ import string
 NULLABLE = {'blank': True, 'null': True}
 
 
-class CustomUser(AbstractUser):
+class User(AbstractUser):
     """Пользователь"""
 
     email = models.EmailField(unique=True, verbose_name='почта')
-    phone_number = models.CharField(max_length=15, unique=True, verbose_name='номер телефона')
+    phone_number = models.CharField(max_length=15, unique=True, default='null', verbose_name='номер телефона')
     country = models.CharField(max_length=100, **NULLABLE, verbose_name='страна')
     city = models.CharField(max_length=150, **NULLABLE, verbose_name='город')
     is_active = models.BooleanField(default=False, verbose_name='активный')
