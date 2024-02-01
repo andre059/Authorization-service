470b99a2879a23fd8cc58c4b819769fc92f6be55
andre059 <andreyperm159@mail.ru>
2024-02-01 15:45:16 +0500

    работает

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
