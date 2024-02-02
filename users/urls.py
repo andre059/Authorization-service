4230d885bd3f72a713b44e484cdf7bae3cab6268
andre059 <andreyperm159@mail.ru>
2024-02-02 21:15:38 +0500

    работает

--- users/urls.py
+++ users/urls.py
@@ -23,4 +23,3 @@ urlpatterns = [
     path('phone-token/', PhoneNumberAndCodeTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', RefreshTokenView.as_view(), name='token_refresh'),
 ]
-
