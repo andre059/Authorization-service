e5c48019bfcec9a3b7a4d948587203e8e06b8a75
andre059 <andreyperm159@mail.ru>
2024-02-12 16:36:26 +0500

    работает

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DATABASES_NAME'),
         'USER': os.getenv('DATABASES_USER'),
-        # 'PASSWORD': os.getenv('DATABASES_PASSWORD'),
+        'PASSWORD': os.getenv('DATABASES_PASSWORD'),
     }
 }
 
